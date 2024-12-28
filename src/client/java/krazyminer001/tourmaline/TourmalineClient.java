929027243240335d1fbdead0b6e375c00346f110
KrazyMiner001 <krazyminer001@outlook.com>
2024-12-28 11:13:33 +0200

    Initial commit

---
+++ src/client/java/krazyminer001/tourmaline/TourmalineClient.java
@@ -0,0 +1,10 @@
+package krazyminer001.tourmaline;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TourmalineClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
